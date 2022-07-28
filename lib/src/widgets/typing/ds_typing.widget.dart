53812461cf4231bf5dd1a5b804f7aa2e92ef0611
Jose Walter <jose.walter@take.net>
2022-07-28 16:15:14 -0300

    implementation of cart typing with animated dots

---
+++ lib/src/widgets/typing/ds_typing.widget.dart
@@ -0,0 +1,39 @@
+import 'package:blip_ds/blip_ds.dart';
+import 'package:flutter/material.dart';
+
+class DsTypingWidget extends StatelessWidget {
+  final DSAlign align;
+  const DsTypingWidget({
+    Key? key,
+    required this.align,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        if (align == DSAlign.right) const Spacer(),
+        SizedBox(
+          width: 150,
+          height: 65,
+          child: DSMessageBubble(
+            align: align,
+            child: Container(
+              transform: Matrix4.translationValues(0.0, 5.0, 0.0),
+              child: DsDotAnimationWidget(
+                numberOfDots: 3,
+                animationDuration: const Duration(
+                  milliseconds: 300,
+                ),
+                color: align == DSAlign.left
+                    ? DSColors.neutralDarkCity
+                    : DSColors.neutralMediumSilver,
+              ),
+            ),
+          ),
+        ),
+        if (align == DSAlign.left) const Spacer(),
+      ],
+    );
+  }
+}
