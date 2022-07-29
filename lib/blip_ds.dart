8b474c16bc5972c353c3f4d2d249dfc16cdd2560
Jose Walter <jose.walter@take.net>
2022-07-29 12:27:09 -0300

    Move widget typing into chat folder

--- lib/blip_ds.dart
+++ lib/blip_ds.dart
@@ -50,4 +50,4 @@ export 'src/themes/texts/styles/ds_caption_text_style.theme.dart'
     show DSCaptionTextStyle;
 
 /// Widgets / Typing
-export 'src/widgets/typing/typing_export.dart';
+export 'src/widgets/chat/typing/typing_export.dart';
