da16dfe001ede816dfc49e36a005b24aab57478d
Andre Rossi <andre.rossi@take.net>
2022-08-03 09:08:39 -0300

    Fix package name

--- lib/src/controllers/chat/ds_file_message_bubble.controller.dart
+++ lib/src/controllers/chat/ds_file_message_bubble.controller.dart
@@ -6,7 +6,7 @@ import 'package:filesize/filesize.dart';
 import 'package:open_filex/open_filex.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:url_launcher/url_launcher_string.dart';
-import 'package:path/path.dart' as p;
+import 'package:path/path.dart' as path_utils;
 
 class DSFileMessageBubbleController extends GetxController {
   final isDownloading = RxBool(false);
@@ -17,17 +17,17 @@ class DSFileMessageBubbleController extends GetxController {
 
   String getAsset(final String filename) {
     const String path = 'assets/images';
-    final String extension = p.extension(filename).isNotEmpty
-        ? p.extension(filename).substring(1)
+    final String extension = path_utils.extension(filename).isNotEmpty
+        ? path_utils.extension(filename).substring(1)
         : '';
 
-    return p.join(path,
+    return path_utils.join(path,
         'file-${extension.length > 3 ? extension.substring(0, 3) : extension}.png');
   }
 
   void openFile(final String filename, final String url) async {
     final String directory = (await getTemporaryDirectory()).path;
-    final String savedFilePath = p.join(directory, filename);
+    final String savedFilePath = path_utils.join(directory, filename);
 
     if (!await io.File(savedFilePath).exists()) {
       final response = await _downloadFile(url, savedFilePath);
