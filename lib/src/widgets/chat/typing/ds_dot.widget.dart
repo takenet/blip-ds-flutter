91c3eaf0a4f3cca41b12a75c28811db9674b72a6
Jose Walter <jose.walter@take.net>
2022-08-04 10:50:57 -0300

    DSDot widget adjustment

--- lib/src/widgets/chat/typing/ds_dot.widget.dart
+++ lib/src/widgets/chat/typing/ds_dot.widget.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
 
 class DSDot extends StatelessWidget {
-  final Color? color;
-  final double? size;
+  final Color color;
+  final double size;
 
+  /// Creates a unique dot with defined size and color
+  ///
   /// Set the dot size and color using the parameters [size] and [color]
   const DSDot({
     Key? key,
-    @required this.color,
-    @required this.size,
+    required this.color,
+    required this.size,
   }) : super(key: key);
 
   @override
