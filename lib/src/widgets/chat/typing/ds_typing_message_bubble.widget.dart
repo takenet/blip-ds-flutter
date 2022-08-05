fc6a17c0e373f4e7d38fe51da2e13b96ba9aca60
Jose Walter <jose.walter@take.net>
2022-08-05 18:48:23 -0300

    Documentation adjustment

--- lib/src/widgets/chat/typing/ds_typing_message_bubble.widget.dart
+++ lib/src/widgets/chat/typing/ds_typing_message_bubble.widget.dart
@@ -1,11 +1,12 @@
 import 'package:blip_ds/blip_ds.dart';
 import 'package:flutter/material.dart';
 
-/// Creates an animation showing that someone is typing something by responding to an interaction
-///
-/// Use [align] left or right to position typing on screen
 class DSTypingAnimationMessageBubble extends StatelessWidget {
   final DSAlign align;
+
+  /// Creates an animation showing that someone is typing something by responding to an interaction
+  ///
+  /// Use [align] left or right to position typing on screen
   const DSTypingAnimationMessageBubble({
     Key? key,
     required this.align,
