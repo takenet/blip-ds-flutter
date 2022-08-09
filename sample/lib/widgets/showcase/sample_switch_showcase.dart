b45043a6275ea74abacbdc5d9ace4335f8156708
Jose Walter <jose.walter@take.net>
2022-08-09 12:32:21 -0300

    Switch Tile sample showcase

--- sample/lib/widgets/showcase/sample_switch_showcase.dart
+++ sample/lib/widgets/showcase/sample_switch_showcase.dart
@@ -1,15 +1,38 @@
 import 'package:blip_ds/blip_ds.dart';
 import 'package:flutter/material.dart';
 
-class SampleSwitchShowcase extends StatelessWidget {
-  const SampleSwitchShowcase({Key? key}) : super(key: key);
+class SampleSwitchShowcase extends StatefulWidget {
+  const SampleSwitchShowcase({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  SampleSwitchShowcaseState createState() => SampleSwitchShowcaseState();
+}
+
+class SampleSwitchShowcaseState extends State<SampleSwitchShowcase> {
+  bool switchValue = true;
 
   @override
   Widget build(BuildContext context) {
     return Column(
+      mainAxisSize: MainAxisSize.min,
       children: [
-        DSSwitchButton(),
-        DSSwitch(),
+        const SizedBox(height: 20),
+        DsSwitchTile(
+          value: switchValue,
+          switchScale: 1.5,
+          onChanged: (value) {
+            setState(() {
+              switchValue = value;
+            });
+          },
+          title: DSBodyText(
+            text: 'Cupertino Switch',
+            overflow: TextOverflow.ellipsis,
+          ),
+        ),
+        const SizedBox(height: 20),
       ],
     );
   }
