489c05bfd98e70cb013e57737b97d8e5ff08eae0
Jose Walter <jose.walter@take.net>
2022-08-11 10:27:31 -0300

    Gradle configuration removing multidex

--- sample/lib/main.dart
+++ sample/lib/main.dart
@@ -1,12 +1,11 @@
 import 'package:blip_ds/blip_ds.dart';
 import 'package:flutter/material.dart';
-import 'package:sample/widgets/showcase/sample_button.showcase.dart';
 import 'package:get/route_manager.dart';
+import 'package:sample/widgets/showcase/sample_message_bubble.showcase.dart';
 
-import 'package:sample/widgets/showcase/sample_message_bubble_showcase.dart';
 import 'package:sample/widgets/showcase/sample_switch_showcase.dart';
-import 'package:sample/widgets/showcase/sample_text_style_showcase.dart';
-import 'package:sample/widgets/showcase/sample_typing_showcase.dart';
+import 'package:sample/widgets/showcase/sample_text_style.showcase.dart';
+import 'package:sample/widgets/showcase/sample_typing.showcase.dart';
 
 void main() {
   runApp(const SampleApp());
@@ -47,9 +46,8 @@ class HomePage extends StatelessWidget {
             const SampleTextStyleShowcase(),
             const Divider(color: DSColors.neutralDarkCity),
             const SampleTypingShowcase(),
-            const Divider(color: DSColors.neutralDarkCity),     
+            const Divider(color: DSColors.neutralDarkCity),
             const SampleSwitchShowcase(),
-
           ],
         ),
       ),
