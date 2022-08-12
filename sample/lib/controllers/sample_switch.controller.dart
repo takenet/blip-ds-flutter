cef3981dff5b282cc0f22580f11437378d6c6c25
Jose Walter <jose.walter@take.net>
2022-08-12 14:28:55 -0300

    Sample Switch controller

---
+++ sample/lib/controllers/sample_switch.controller.dart
@@ -0,0 +1,14 @@
+import 'package:get/get.dart';
+
+/// GetX Controller for holding Switch's current value
+class SampleSwitchController extends GetxController {
+  SampleSwitchController();
+
+  RxBool onSwitch = false.obs;
+  RxBool onSwitchTile = true.obs;
+  RxBool onSwitchTileDisabled = true.obs;
+
+  /// Swap true/false & save it to observable
+  void toggleSwitch() => onSwitch.value = !onSwitch.value;
+  void toggleSwitchTile() => onSwitchTile.value = !onSwitchTile.value;
+}
