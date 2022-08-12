0445450510e9072f453beed2fa8bafed5ca29d41
Jose Walter <jose.walter@take.net>
2022-08-12 13:03:48 -0300

    Reworked SwitchTile

--- lib/src/widgets/switch/ds_switch_tile.widget.dart
+++ lib/src/widgets/switch/ds_switch_tile.widget.dart
@@ -5,60 +5,55 @@ import 'package:flutter/material.dart';
 class DSSwitchTile extends StatelessWidget {
   /// Create a tile widget with a switch button
   ///
-  ///It is mandatory to inform three parameters: [value], enabling or disabling the switch, [onChanged], the event that will be executed when changing the value, and [title], which is the title of the tile.
+  ///It is mandatory to inform three parameters: [isActive], enabling or disabling the switch, [onChanged], the event that will be executed when changing the value, and [title], which is the title of the tile.
   const DSSwitchTile({
     Key? key,
-    required this.value,
+    required this.isActive,
     required this.onChanged,
     required this.title,
-    this.switchActiveColor = DSColors.primaryNight,
-    this.switchInactiveColor = DSColors.neutralMediumSilver,
     this.leading,
-    this.isEnabled = true,
+    this.isEnabled = false,
     this.contentPadding,
   }) : super(key: key);
 
   /// current state of the switch widget.
-  final bool value;
+  final bool isActive;
 
-  /// Similar to [Switch.onChanged] and [CupertinoSwitch.onChanged].
+  /// Callback performed when changing status [isActive]
   final ValueChanged<bool> onChanged;
 
+  /// Switchtile title to be shown referring to the switch
   final Widget? title;
-
-  /// Switch button and title line color when [value] is true.
-  final Color? switchActiveColor;
-
-  /// Switch button color when [value] is false.
-  final Color? switchInactiveColor;
 
   /// Make it possible to use an image, icon, avatar or other widget together with the switch before the title.
   final Widget? leading;
 
-  /// Enables or disables the widget by changing the color of the switch.
+  /// Enables or disables the widget switch by changing the color of the switch.
   final bool isEnabled;
 
   /// Change a padding tile content.
   final EdgeInsetsGeometry? contentPadding;
 
+  /// Referring to [inactiveColor] for  [isActive] false,
+  final Color inactiveColor = DSColors.neutralMediumSilver;
+
+  /// Color for background of the switch widget when [isActive] is true.
+  final Color activeColor = DSColors.primaryNight;
+
   @override
   Widget build(BuildContext context) {
-    return ListTileTheme.merge(
-      selectedColor: switchActiveColor,
-      child: ListTile(
-        enabled: isEnabled,
-        onTap: () => onChanged(!value),
-        leading: leading,
-        title: title,
-        trailing: DSSwitch(
-          activeColor: isEnabled ? switchActiveColor : switchInactiveColor,
-          inactiveColor: switchInactiveColor,
-          value: value,
-          onChanged: onChanged,
-        ),
-        contentPadding: contentPadding,
-        selected: value,
+    return ListTile(
+      enabled: isEnabled,
+      onTap: () => onChanged(!isActive),
+      leading: leading,
+      title: title,
+      trailing: DSSwitch(
+        isActive: isActive,
+        isEnabled: isEnabled,
+        onChanged: onChanged,
       ),
+      contentPadding: contentPadding,
+      selected: isActive,
     );
   }
 }
