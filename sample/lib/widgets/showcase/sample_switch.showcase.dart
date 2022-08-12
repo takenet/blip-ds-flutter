840a502ba4df5436a8ac8b898c3cd108523ae1e6
Jose Walter <jose.walter@take.net>
2022-08-12 14:32:23 -0300

    Sample Switch com Getx

--- sample/lib/widgets/showcase/sample_switch.showcase.dart
+++ sample/lib/widgets/showcase/sample_switch.showcase.dart
@@ -1,44 +1,47 @@
 import 'package:blip_ds/blip_ds.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:sample/controllers/sample_switch.controller.dart';
 
-class SampleSwitchShowcase extends StatefulWidget {
-  const SampleSwitchShowcase({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  SampleSwitchShowcaseState createState() => SampleSwitchShowcaseState();
-}
-
-class SampleSwitchShowcaseState extends State<SampleSwitchShowcase> {
-  bool switchValue = true;
+class SampleSwitchShowcase extends StatelessWidget {
+  const SampleSwitchShowcase({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    SampleSwitchController controller = Get.put(SampleSwitchController());
+
     return Column(
       mainAxisSize: MainAxisSize.min,
       children: [
         const SizedBox(height: 20),
-        DSSwitchTile(
-          value: switchValue,
-          onChanged: (value) {
-            setState(() {
-              switchValue = value;
-            });
-          },
-          title: DSBodyText(
-            text: 'Cupertino Switch dfzgs dfgsd fgsdfg sdfg',
-            overflow: TextOverflow.ellipsis,
+        Obx(
+          () => DSSwitchTile(
+            isActive: controller.onSwitchTile.value,
+            onChanged: (value) => controller.toggleSwitchTile(),
+            title: DSBodyText(
+              text: 'Label switch',
+              overflow: TextOverflow.ellipsis,
+            ),
           ),
         ),
         const SizedBox(height: 20),
-        DSSwitch(
-          value: switchValue,
-          onChanged: (value) {
-            setState(() {
-              switchValue = value;
-            });
-          },
+        Obx(
+          () => DSSwitchTile(
+            isEnabled: false,
+            isActive: controller.onSwitchTileDisabled.value,
+            onChanged: (value) => controller.onSwitchTileDisabled,
+            title: DSBodyText(
+              text: 'Label switch',
+              overflow: TextOverflow.ellipsis,
+            ),
+          ),
+        ),
+        const SizedBox(height: 20),
+        Obx(
+          () => DSSwitch(
+            onChanged: (value) => controller.toggleSwitch(),
+            isActive: controller.onSwitch.value,
+          ),
         ),
         const SizedBox(height: 20),
       ],
