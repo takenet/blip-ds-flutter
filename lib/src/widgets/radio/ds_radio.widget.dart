91f975b689f29d1b8f6acc5ddde210cf2fe113e3
Jose Walter <jose.walter@take.net>
2022-08-19 16:32:15 -0300

    Flutter native radio button, restyled

---
+++ lib/src/widgets/radio/ds_radio.widget.dart
@@ -0,0 +1,321 @@
+// Copyright 2014 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:blip_ds/blip_ds.dart';
+import 'package:flutter/material.dart';
+
+/// A material design radio button custom.
+///
+/// Used to select between a number of mutually exclusive values. When one radio
+/// button in a group is selected, the other radio buttons in the group cease to
+/// be selected. The values are of type `T`, the type parameter of the [Radio]
+/// class. Enums are commonly used for this purpose.
+///
+/// The radio button itself does not maintain any state. Instead, selecting the
+/// radio invokes the [onChanged] callback, passing [value] as a parameter. If
+/// [groupValue] and [value] match, this radio will be selected. Most widgets
+/// will respond to [onChanged] by calling [State.setState] to update the
+/// radio button's [groupValue].
+///
+/// {@tool dartpad}
+/// Here is an example of Radio widgets wrapped in ListTiles, which is similar
+/// to what you could get with the RadioListTile widget.
+///
+/// The currently selected character is passed into `groupValue`, which is
+/// maintained by the example's `State`. In this case, the first `Radio`
+/// will start off selected because `_character` is initialized to
+/// `SingingCharacter.lafayette`.
+///
+/// If the second radio button is pressed, the example's state is updated
+/// with `setState`, updating `_character` to `SingingCharacter.jefferson`.
+/// This causes the buttons to rebuild with the updated `groupValue`, and
+/// therefore the selection of the second button.
+///
+/// Requires one of its ancestors to be a [Material] widget.
+///
+/// ** See code in examples/api/lib/material/radio/radio.0.dart **
+/// {@end-tool}
+///
+/// See also:
+///
+///  * [RadioListTile], which combines this widget with a [ListTile] so that
+///    you can give the radio button a label.
+///  * [Slider], for selecting a value in a range.
+///  * [Checkbox] and [Switch], for toggling a particular value on or off.
+///  * <https://material.io/design/components/selection-controls.html#radio-buttons>
+class DSRadio<T> extends Radio<T> {
+  /// Creates a material design radio button.
+  ///
+  /// The radio button itself does not maintain any state. Instead, when the
+  /// radio button is selected, the widget calls the [onChanged] callback. Most
+  /// widgets that use a radio button will listen for the [onChanged] callback
+  /// and rebuild the radio button with a new [groupValue] to update the visual
+  /// appearance of the radio button.
+  ///
+  /// The following arguments are required:
+  ///
+  /// * [value] and [groupValue] together determine whether the radio button is
+  ///   selected.
+  /// * [onChanged] is called when the user selects this radio button.
+  const DSRadio({
+    Key? key,
+    required super.value,
+    required super.groupValue,
+    required super.onChanged,
+    super.mouseCursor,
+    super.toggleable = false,
+    super.activeColor = DSColors.primaryNight,
+    super.fillColor,
+    super.focusColor,
+    super.hoverColor,
+    super.overlayColor,
+    super.splashRadius,
+    super.materialTapTargetSize,
+    super.visualDensity,
+    super.focusNode,
+    super.autofocus = false,
+    this.isEnabled = true,
+  }) : super(
+          key: key,
+        );
+
+  /// The [isEnabled] parameter allows disabling the radio button according to its true/false value.
+  final bool isEnabled;
+
+  bool get _selected => value == groupValue;
+
+  @override
+  State<DSRadio<T>> createState() => _RadioState<T>();
+}
+
+class _RadioState<T> extends State<DSRadio<T>>
+    with TickerProviderStateMixin, ToggleableStateMixin {
+  final _RadioPainter _painter = _RadioPainter();
+
+  void _handleChanged(bool? selected) {
+    if (widget.isEnabled) {
+      if (selected == null) {
+        widget.onChanged!(null);
+        return;
+      }
+      if (selected) {
+        widget.onChanged!(widget.value);
+      }
+    }
+  }
+
+  @override
+  void didUpdateWidget(DSRadio<T> oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (widget._selected != oldWidget._selected) {
+      animateToValue();
+    }
+  }
+
+  @override
+  void dispose() {
+    _painter.dispose();
+    super.dispose();
+  }
+
+  @override
+  ValueChanged<bool?>? get onChanged =>
+      widget.onChanged != null ? _handleChanged : null;
+
+  @override
+  bool get tristate => widget.toggleable;
+
+  @override
+  bool? get value => widget._selected;
+
+  MaterialStateProperty<Color?> get _widgetFillColor {
+    return MaterialStateProperty.resolveWith((Set<MaterialState> states) {
+      if (states.contains(MaterialState.disabled)) {
+        return null;
+      }
+      if (states.contains(MaterialState.selected)) {
+        return widget.activeColor;
+      }
+      return null;
+    });
+  }
+
+  MaterialStateProperty<Color> get _defaultFillColor {
+    final ThemeData themeData = Theme.of(context);
+    return MaterialStateProperty.resolveWith((Set<MaterialState> states) {
+      if (states.contains(MaterialState.disabled)) {
+        return themeData.disabledColor;
+      }
+      if (states.contains(MaterialState.selected)) {
+        return themeData.toggleableActiveColor;
+      }
+      return themeData.unselectedWidgetColor;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    assert(debugCheckHasMaterial(context));
+    final ThemeData themeData = Theme.of(context);
+    final MaterialTapTargetSize effectiveMaterialTapTargetSize =
+        widget.materialTapTargetSize ??
+            themeData.radioTheme.materialTapTargetSize ??
+            themeData.materialTapTargetSize;
+    final VisualDensity effectiveVisualDensity = widget.visualDensity ??
+        themeData.radioTheme.visualDensity ??
+        themeData.visualDensity;
+    Size size;
+    switch (effectiveMaterialTapTargetSize) {
+      case MaterialTapTargetSize.padded:
+        size = const Size(kMinInteractiveDimension, kMinInteractiveDimension);
+        break;
+      case MaterialTapTargetSize.shrinkWrap:
+        size = const Size(
+            kMinInteractiveDimension - 8.0, kMinInteractiveDimension - 8.0);
+        break;
+    }
+    size += effectiveVisualDensity.baseSizeAdjustment;
+
+    final MaterialStateProperty<MouseCursor> effectiveMouseCursor =
+        MaterialStateProperty.resolveWith<MouseCursor>(
+            (Set<MaterialState> states) {
+      return MaterialStateProperty.resolveAs<MouseCursor?>(
+              widget.mouseCursor, states) ??
+          themeData.radioTheme.mouseCursor?.resolve(states) ??
+          MaterialStateProperty.resolveAs<MouseCursor>(
+              MaterialStateMouseCursor.clickable, states);
+    });
+
+    _painter.isEnabled = widget.isEnabled;
+
+    // Colors need to be resolved in selected and non selected states separately
+    // so that they can be lerped between.
+    final Set<MaterialState> activeStates = states..add(MaterialState.selected);
+    final Set<MaterialState> inactiveStates = states
+      ..remove(MaterialState.selected);
+    final Color effectiveActiveColor =
+        widget.fillColor?.resolve(activeStates) ??
+            _widgetFillColor.resolve(activeStates) ??
+            themeData.radioTheme.fillColor?.resolve(activeStates) ??
+            _defaultFillColor.resolve(activeStates);
+    final Color effectiveInactiveColor =
+        widget.fillColor?.resolve(inactiveStates) ??
+            _widgetFillColor.resolve(inactiveStates) ??
+            themeData.radioTheme.fillColor?.resolve(inactiveStates) ??
+            _defaultFillColor.resolve(inactiveStates);
+
+    final Set<MaterialState> focusedStates = states..add(MaterialState.focused);
+    final Color effectiveFocusOverlayColor =
+        widget.overlayColor?.resolve(focusedStates) ??
+            widget.focusColor ??
+            themeData.radioTheme.overlayColor?.resolve(focusedStates) ??
+            themeData.focusColor;
+
+    final Set<MaterialState> hoveredStates = states..add(MaterialState.hovered);
+    final Color effectiveHoverOverlayColor =
+        widget.overlayColor?.resolve(hoveredStates) ??
+            widget.hoverColor ??
+            themeData.radioTheme.overlayColor?.resolve(hoveredStates) ??
+            themeData.hoverColor;
+
+    final Set<MaterialState> activePressedStates = activeStates
+      ..add(MaterialState.pressed);
+    final Color effectiveActivePressedOverlayColor =
+        widget.overlayColor?.resolve(activePressedStates) ??
+            themeData.radioTheme.overlayColor?.resolve(activePressedStates) ??
+            effectiveActiveColor.withAlpha(kRadialReactionAlpha);
+
+    final Set<MaterialState> inactivePressedStates = inactiveStates
+      ..add(MaterialState.pressed);
+    final Color effectiveInactivePressedOverlayColor =
+        widget.overlayColor?.resolve(inactivePressedStates) ??
+            themeData.radioTheme.overlayColor?.resolve(inactivePressedStates) ??
+            effectiveActiveColor.withAlpha(kRadialReactionAlpha);
+
+    return Semantics(
+      inMutuallyExclusiveGroup: true,
+      checked: widget._selected,
+      child: buildToggleable(
+        focusNode: widget.focusNode,
+        autofocus: widget.autofocus,
+        mouseCursor: effectiveMouseCursor,
+        size: size,
+        painter: _painter
+          ..position = position
+          ..reaction = reaction
+          ..reactionFocusFade = reactionFocusFade
+          ..reactionHoverFade = reactionHoverFade
+          ..inactiveReactionColor = effectiveInactivePressedOverlayColor
+          ..reactionColor = effectiveActivePressedOverlayColor
+          ..hoverColor = effectiveHoverOverlayColor
+          ..focusColor = effectiveFocusOverlayColor
+          ..splashRadius = widget.splashRadius ??
+              themeData.radioTheme.splashRadius ??
+              kRadialReactionRadius
+          ..downPosition = downPosition
+          ..isFocused = states.contains(MaterialState.focused)
+          ..isHovered = states.contains(MaterialState.hovered)
+          ..activeColor = effectiveActiveColor
+          ..inactiveColor = effectiveInactiveColor,
+      ),
+    );
+  }
+}
+
+class _RadioPainter extends ToggleablePainter {
+  final double _kOuterRadius = 24.0 * 0.65;
+  final double _kInnerRadius = 12.0 * 0.65;
+  final double _kBachgroundRadius = 22.0 * 0.65;
+
+  bool isEnabled = true;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    paintRadialReaction(canvas: canvas, origin: size.center(Offset.zero));
+
+    final Offset center = (Offset.zero & size).center;
+
+    if (isEnabled) {
+      // Outer circle
+      final Paint paint = Paint()
+        ..color = const Color.fromARGB(255, 0, 0, 0)
+        ..style = PaintingStyle.stroke
+        ..strokeWidth = 2.0;
+      canvas.drawCircle(center, _kOuterRadius, paint);
+
+      // Inner circle
+      if (!position.isDismissed) {
+        paint.color = Color.lerp(inactiveColor, activeColor, position.value)!;
+
+        paint.style = PaintingStyle.fill;
+        canvas.drawCircle(center, _kInnerRadius * position.value, paint);
+      }
+    } else {
+      // Outer circle background Disabled
+      final Paint paintDisabled = Paint()
+        ..color = DSColors.neutralMediumWave
+        ..style = PaintingStyle.fill;
+
+      // Outer circle Disabled
+      final Paint paint = Paint()
+        ..color = DSColors.neutralDarkRooftop
+        ..style = PaintingStyle.stroke
+        ..strokeWidth = 2.0;
+      canvas.drawCircle(center, _kOuterRadius, paint);
+
+      canvas.drawCircle(center, _kBachgroundRadius, paintDisabled);
+
+      // Inner circle
+      if (!position.isDismissed) {
+        paint.color = Color.lerp(
+            inactiveColor, DSColors.neutralDarkRooftop, position.value)!;
+
+        paint.strokeWidth = _kInnerRadius;
+
+        paint.style = PaintingStyle.fill;
+        canvas.drawCircle(center, _kInnerRadius * position.value, paint);
+      }
+    }
+  }
+}
