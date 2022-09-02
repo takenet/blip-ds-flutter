021d57cb1285bc9e2ffada52fefbf7f7e5098fcb
Jose Walter <jose.walter@take.net>
2022-09-02 17:19:49 -0300

    DSVideoMessageBubble documentation

--- lib/src/widgets/chat/ds_video_message_bubble.widget.dart
+++ lib/src/widgets/chat/ds_video_message_bubble.widget.dart
@@ -2,13 +2,28 @@ import 'package:blip_ds/blip_ds.dart';
 import 'package:flutter/material.dart';
 
 class DSVideoMessageBubble extends StatelessWidget {
+  /// Aligns the card to the right or left according to the value assigned to [align] which can be [DSAlign.right] or [DSAlign.left].
   final DSAlign align;
+
+  /// URL of the video that will be played when clicking on the card
   final String urlVideo;
+
   final List<DSBorderRadius> borderRadius;
+
+  /// Video title to be shown below the thumbnail
   final String videoTitle;
+
+  /// Text associated with the video context to be shown below the title.
   final String? imageText;
+
+  /// AppBar title above the video screen that also serves to close, returning to the previous screen.
   final String appBarText;
 
+  /// Card for the purpose of triggering a video to play.
+  ///
+  /// This widget is intended to display a video card from a url passed in the [urlVideo] parameter.
+  /// The card can be positioned to the right or to the left by the [align] parameter.
+  /// The card may also contain a title and text referring to the context of the video to be played.
   const DSVideoMessageBubble({
     super.key,
     required this.align,
