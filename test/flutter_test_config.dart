f2d9842310eabf7a346fa18a3ba98028fc92cab9
Marcelo Amaro <marcelo.amaro@take.net>
2022-10-11 11:39:48 -0300

    Revert "Unload app fonts from test goldens"
    This reverts commit 664ebeac262530cba96dba76da5a996f4fcb4f28.

--- test/flutter_test_config.dart
+++ test/flutter_test_config.dart
@@ -1,13 +1,8 @@
 import 'dart:async';
 
-// import 'package:golden_toolkit/golden_toolkit.dart';
+import 'package:golden_toolkit/golden_toolkit.dart';
 
 Future<void> testExecutable(FutureOr<void> Function() testMain) async {
-  // When loading the app fonts, CI (GitHub Actions) will fail because of the OS font alias.
-  // See: https://github.com/flutter/flutter/issues/56383
-  //
-  // TODO: Load the app fonts and make sure that it'll pass through tests.yml workflow.
-  // await loadAppFonts();
-
+  await loadAppFonts();
   return testMain();
 }
