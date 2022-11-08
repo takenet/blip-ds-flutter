ca813bb6f2df16ca57661761abbaf5b24b0a4c31
leonardo.gabriel <leonardo.gabriel@take.net>
2022-11-08 16:34:08 -0300

    [DSCard] Changed imports

--- lib/src/widgets/utils/ds_card.widget.dart
+++ lib/src/widgets/utils/ds_card.widget.dart
@@ -1,17 +1,18 @@
-import 'package:blip_ds/src/enums/ds_align.enum.dart';
-import 'package:blip_ds/src/enums/ds_border_radius.enum.dart';
-import 'package:blip_ds/src/models/ds_message_bubble_style.model.dart';
-import 'package:blip_ds/src/widgets/chat/audio/ds_audio_message_bubble.widget.dart';
-import 'package:blip_ds/src/widgets/chat/ds_carrousel.widget.dart';
-import 'package:blip_ds/src/widgets/chat/ds_file_message_bubble.widget.dart';
-import 'package:blip_ds/src/widgets/chat/ds_image_message_bubble.widget.dart';
-import 'package:blip_ds/src/widgets/chat/ds_text_message_bubble.widget.dart';
-import 'package:blip_ds/src/widgets/chat/ds_unsupported_content_message_bubble.widget.dart';
-import 'package:blip_ds/src/widgets/chat/ds_video_message_bubble.widget.dart';
-import 'package:blip_ds/src/widgets/chat/ds_weblink.widget.dart';
 import 'package:flutter/material.dart';
+
+import '../../enums/ds_align.enum.dart';
+import '../../enums/ds_border_radius.enum.dart';
 import '../../models/ds_document_select.model.dart';
+import '../../models/ds_message_bubble_style.model.dart';
 import '../../utils/ds_message_content_type.util.dart';
+import '../chat/audio/ds_audio_message_bubble.widget.dart';
+import '../chat/ds_carrousel.widget.dart';
+import '../chat/ds_file_message_bubble.widget.dart';
+import '../chat/ds_image_message_bubble.widget.dart';
+import '../chat/ds_text_message_bubble.widget.dart';
+import '../chat/ds_unsupported_content_message_bubble.widget.dart';
+import '../chat/ds_video_message_bubble.widget.dart';
+import '../chat/ds_weblink.widget.dart';
 
 /// A Design System widget used to display a Design System's widget based in LIME protocol content types
 class DSCard extends StatelessWidget {
