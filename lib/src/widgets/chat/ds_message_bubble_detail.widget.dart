12d48541ada0eaa9ccbd8e8a3ef806792df6c414
RaulRodrigo06 <raulrodrigo6@gmail.com>
2023-03-16 18:48:26 -0300

    refactor: adding detailWidget on message bubble detail

--- lib/src/widgets/chat/ds_message_bubble_detail.widget.dart
+++ lib/src/widgets/chat/ds_message_bubble_detail.widget.dart
@@ -3,7 +3,8 @@ import 'package:flutter/material.dart';
 
 /// A Design System widget used to display a date and a [DSDeliveryReportIcon] at a message.
 class DSMessageBubbleDetail extends StatelessWidget {
-  final String date;
+  final String? date;
+  final Widget? detailWidget;
   final DSAlign align;
   final DSDeliveryReportStatus deliveryStatus;
   final DSMessageBubbleStyle style;
@@ -12,12 +13,14 @@ class DSMessageBubbleDetail extends StatelessWidget {
   /// Creates a new Design System's [DSMessageBubbleDetail]
   const DSMessageBubbleDetail({
     Key? key,
-    required this.date,
     required this.align,
     required this.deliveryStatus,
     required this.style,
     this.showMessageStatus = true,
-  }) : super(key: key);
+    this.date,
+    this.detailWidget,
+  })  : assert((date != null || detailWidget != null)),
+        super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -26,24 +29,36 @@ class DSMessageBubbleDetail extends StatelessWidget {
           ? MainAxisAlignment.end
           : MainAxisAlignment.start,
       children: [
-        Container(
-          padding: const EdgeInsets.fromLTRB(0, 5.0, 0, 0),
-          child: Row(
-            children: [
-              if (showMessageStatus && align == DSAlign.right) ...[
-                DSDeliveryReportIcon(deliveryStatus: deliveryStatus),
-                const SizedBox(
-                  width: 6.0,
-                ),
+        Expanded(
+          child: Container(
+            padding: const EdgeInsets.fromLTRB(0, 5.0, 0, 0),
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                if (showMessageStatus && align == DSAlign.right) ...[
+                  DSDeliveryReportIcon(deliveryStatus: deliveryStatus),
+                  const SizedBox(
+                    width: 6.0,
+                  ),
+                ],
+                _getDetailWidget(),
               ],
-              DSCaptionSmallText(
-                date,
-                color: style.isPageBackgroundLight ? DSColors.neutralDarkCity : DSColors.neutralLightSnow, 
-              )
-            ],
+            ),
           ),
         ),
       ],
+    );
+  }
+
+  Widget _getDetailWidget() {
+    if (detailWidget != null) {
+      return Expanded(child: detailWidget!);
+    }
+    return DSCaptionSmallText(
+      date,
+      color: style.isPageBackgroundLight
+          ? DSColors.neutralDarkCity
+          : DSColors.neutralLightSnow,
     );
   }
 }
