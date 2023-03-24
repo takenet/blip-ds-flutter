049ad5d63d1d31503ec1927db737ed22197b21ce
Marcelo Amaro <marcelo.amaro@take.net>
2023-03-24 13:53:09 -0300

    Change DSAudioSpeedButton to extend InkWell

--- lib/src/widgets/chat/audio/ds_audio_speed_button.widget.dart
+++ lib/src/widgets/chat/audio/ds_audio_speed_button.widget.dart
@@ -3,39 +3,36 @@ import 'package:flutter/material.dart';
 import '../../../themes/texts/utils/ds_font_weights.theme.dart';
 import '../../texts/ds_caption_text.widget.dart';
 
-class DSAudioSpeedButton extends StatelessWidget {
-  final VoidCallback onTap;
+class DSAudioSpeedButton extends InkWell {
   final Color borderColor;
   final Color color;
   final String text;
 
-  const DSAudioSpeedButton({
-    Key? key,
-    required this.onTap,
+  DSAudioSpeedButton({
+    super.key,
+    required super.onTap,
     required this.color,
     required this.borderColor,
     required this.text,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () => onTap(),
-      child: Container(
-        width: 44.0,
-        height: 46.0,
-        decoration: BoxDecoration(
-          borderRadius: const BorderRadius.all(Radius.circular(16.0)),
-          border: Border.all(color: borderColor),
-        ),
-        child: Center(
-          child: DSCaptionText(
-            text,
-            color: color,
-            fontWeight: DSFontWeights.bold,
+  }) : super(
+          child: Container(
+            width: 44.0,
+            height: 46.0,
+            decoration: BoxDecoration(
+              borderRadius: const BorderRadius.all(
+                Radius.circular(16.0),
+              ),
+              border: Border.all(
+                color: borderColor,
+              ),
+            ),
+            child: Center(
+              child: DSCaptionText(
+                text,
+                color: color,
+                fontWeight: DSFontWeights.bold,
+              ),
+            ),
           ),
-        ),
-      ),
-    );
-  }
+        );
 }
