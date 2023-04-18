2fbe0cffdb720fc1d4411b78683841668f3f43ad
Paolla Lira <paollalira@outlook.com>
2023-04-18 18:14:14 -0300

    Input number package

---
+++ lib/src/widgets/fields/ds_input_phone.widget.dart
@@ -0,0 +1,34 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/material.dart';
+import 'package:intl_phone_number_input/intl_phone_number_input.dart';
+
+import 'package:blip_ds/src/widgets/fields/ds_input_decoration.widget.dart';
+
+class DSInputPhone extends StatelessWidget {
+  final String? hintText;
+  final String initialCountry = 'BR';
+  final PhoneNumber number = PhoneNumber(isoCode: 'BR');
+  final TextEditingController controller = TextEditingController();
+  DSInputPhone({
+    Key? key,
+    this.hintText,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return DSInputDecoration(
+        child: InternationalPhoneNumberInput(
+      hintText: hintText,
+      initialValue: number,
+      keyboardType:
+          const TextInputType.numberWithOptions(signed: true, decimal: true),
+      textFieldController: controller,
+      
+      formatInput: true, //??
+      maxLength: 15,
+      onInputChanged: (PhoneNumber number) {
+        number.phoneNumber;
+      },
+    ));
+  }
+}
---
+++ sample/lib/widgets/showcase/sample_input_phone.showcase.dart
@@ -0,0 +1,12 @@
+import 'package:blip_ds/blip_ds.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class SampleInputPhoneShowcase extends StatelessWidget {
+  const SampleInputPhoneShowcase({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return DSInputPhone();
+  }
+}
