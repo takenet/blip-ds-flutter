4836cdf555f1d145047efc6830379687bb75409f
RaulRodrigo06 <raulrodrigo6@gmail.com>
2023-04-19 15:29:47 -0300

    fix: treat focus on design system

--- lib/src/widgets/fields/ds_text_form_field.widget.dart
+++ lib/src/widgets/fields/ds_text_form_field.widget.dart
@@ -1,17 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 import '../../themes/colors/ds_colors.theme.dart';
 import '../../themes/texts/styles/ds_body_text_style.theme.dart';
 import '../../utils/ds_utils.util.dart';
 import 'ds_input_decoration.widget.dart';
 
-class DSTextFormField extends StatelessWidget {
-  DSTextFormField({
+class DSTextFormField extends StatefulWidget {
+  const DSTextFormField({
     super.key,
     required this.controller,
     required this.hint,
     required this.onChanged,
-    required this.hasFocus,
     this.textInputAction = TextInputAction.send,
     this.onTap,
     this.onTapOutside,
@@ -35,81 +35,91 @@ class DSTextFormField extends StatelessWidget {
   final bool obscureText;
   final FocusNode? focusNode;
   final bool isEnabled;
-  final bool hasFocus;
-
-  final _scrollController = ScrollController();
 
   @override
-  Widget build(BuildContext context) => DSInputDecoration(
-        hasFocus: focusNode?.hasFocus,
-        padding: const EdgeInsets.only(
-          left: 12.0,
-          right: 6.0,
-          top: 10.0,
-          bottom: 10.0,
-        ),
-        child: Scrollbar(
-          controller: _scrollController,
-          radius: const Radius.circular(5),
-          child: Padding(
+  State<DSTextFormField> createState() => _DSTextFormFieldState();
+}
+
+class _DSTextFormFieldState extends State<DSTextFormField> {
+  final _scrollController = ScrollController();
+  final hasFocus = RxBool(false);
+
+  @override
+  Widget build(BuildContext context) => Obx(
+        () => Focus(
+          focusNode: widget.focusNode,
+          onFocusChange: hasFocus,
+          child: DSInputDecoration(
+            hasFocus: hasFocus.value,
             padding: const EdgeInsets.only(
+              left: 12.0,
               right: 6.0,
+              top: 10.0,
+              bottom: 10.0,
             ),
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.end,
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Flexible(
-                  child: ConstrainedBox(
-                    constraints: const BoxConstraints(
-                      maxHeight: 100.0,
-                    ),
-                    child: TextFormField(
-                      controller: controller,
-                      scrollController: _scrollController,
-                      enabled: isEnabled,
-                      obscureText: obscureText,
-                      focusNode: focusNode,
-                      style: const DSBodyTextStyle(),
-                      decoration: InputDecoration(
-                        isDense: true,
-                        contentPadding: EdgeInsets.zero,
-                        border: InputBorder.none,
-                        focusedBorder: InputBorder.none,
-                        enabledBorder: InputBorder.none,
-                        errorBorder: InputBorder.none,
-                        disabledBorder: InputBorder.none,
-                        hintText: hint,
-                        hintStyle: const DSBodyTextStyle(
-                          color: DSColors.neutralMediumSilver,
+            child: Scrollbar(
+              controller: _scrollController,
+              radius: const Radius.circular(5),
+              child: Padding(
+                padding: const EdgeInsets.only(
+                  right: 6.0,
+                ),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Flexible(
+                      child: ConstrainedBox(
+                        constraints: const BoxConstraints(
+                          maxHeight: 100.0,
+                        ),
+                        child: TextFormField(
+                          controller: widget.controller,
+                          scrollController: _scrollController,
+                          enabled: widget.isEnabled,
+                          obscureText: widget.obscureText,
+                          style: const DSBodyTextStyle(),
+                          decoration: InputDecoration(
+                            isDense: true,
+                            contentPadding: EdgeInsets.zero,
+                            border: InputBorder.none,
+                            focusedBorder: InputBorder.none,
+                            enabledBorder: InputBorder.none,
+                            errorBorder: InputBorder.none,
+                            disabledBorder: InputBorder.none,
+                            hintText: widget.hint,
+                            hintStyle: const DSBodyTextStyle(
+                              color: DSColors.neutralMediumSilver,
+                            ),
+                          ),
+                          cursorColor: DSColors.primaryMain,
+                          cursorHeight: 20.0,
+                          textCapitalization: widget.textCapitalization,
+                          textInputAction: widget.textInputAction,
+                          maxLines: widget.obscureText ? 1 : widget.maxLines,
+                          onChanged: widget.onChanged,
+                          onTap: widget.onTap,
+                          onTapOutside: widget.onTapOutside,
                         ),
                       ),
-                      cursorColor: DSColors.primaryMain,
-                      cursorHeight: 20.0,
-                      textCapitalization: textCapitalization,
-                      textInputAction: textInputAction,
-                      maxLines: obscureText ? 1 : maxLines,
-                      onChanged: onChanged,
-                      onTap: onTap,
-                      onTapOutside: onTapOutside,
                     ),
-                  ),
+                    if (widget.showEmojiButton)
+                      IconButton(
+                        /// TODO: Implement to open emoji keyboard!!
+                        onPressed: () {},
+                        padding: EdgeInsets.zero,
+                        splashRadius: 15,
+                        constraints: const BoxConstraints(),
+                        icon: Image.asset(
+                          'assets/images/emoji.png',
+                          package: DSUtils.packageName,
+                          height: 20.5,
+                          width: 20.5,
+                        ),
+                      )
+                  ],
                 ),
-                if (showEmojiButton)
-                  IconButton(
-                    /// TODO: Implement to open emoji keyboard!!
-                    onPressed: () {},
-                    padding: EdgeInsets.zero,
-                    splashRadius: 15,
-                    constraints: const BoxConstraints(),
-                    icon: Image.asset(
-                      'assets/images/emoji.png',
-                      package: DSUtils.packageName,
-                      height: 20.5,
-                      width: 20.5,
-                    ),
-                  )
-              ],
+              ),
             ),
           ),
         ),
