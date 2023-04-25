86de2bf31cff9936d3c4ae710dbbc597adee3265
Paolla Lira <paollalira@outlook.com>
2023-04-25 14:06:48 -0300

    Input

--- lib/src/widgets/fields/ds_input_phone.widget.dart
+++ lib/src/widgets/fields/ds_input_phone.widget.dart
@@ -1,8 +1,9 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:blip_ds/blip_ds.dart';
 import 'package:flutter/material.dart';
 import 'package:intl_phone_number_input/intl_phone_number_input.dart';
 
-import 'package:blip_ds/src/widgets/fields/ds_input_decoration.widget.dart';
+//import 'package:blip_ds/src/widgets/fields/ds_input_decoration.widget.dart';
 
 class DSInputPhone extends StatelessWidget {
   final String? hintText;
@@ -10,24 +11,49 @@ class DSInputPhone extends StatelessWidget {
   final PhoneNumber number = PhoneNumber(isoCode: 'BR');
   final TextEditingController controller = TextEditingController();
   DSInputPhone({
-    Key? key,
+    super.key,
     this.hintText,
-  }) : super(key: key);
+  });
 
   @override
   Widget build(BuildContext context) {
-    return DSInputDecoration(
-        child: InternationalPhoneNumberInput(
-      searchBoxDecoration: const InputDecoration(hoverColor: Colors.red), inputBorder: InputBorder.,
-      hintText: hintText,
-      initialValue: number,
-      keyboardType:
-          const TextInputType.numberWithOptions(signed: true, decimal: true),
-      textFieldController: controller,
-      maxLength: 15,
-      onInputChanged: (PhoneNumber number) {
-        number.phoneNumber;
-      },
-    ));
+    return Container(
+      decoration: const BoxDecoration(
+        border: Border(
+          bottom: BorderSide(color: DSColors.neutralMediumWave),
+          top: BorderSide(color: DSColors.neutralMediumWave),
+          left: BorderSide(color: DSColors.neutralMediumWave),
+          right: BorderSide(color: DSColors.neutralMediumWave),
+        ),
+        borderRadius: BorderRadius.all(
+          Radius.circular(8.0),
+        ),
+      ),
+      child: InternationalPhoneNumberInput(
+        searchBoxDecoration: const InputDecoration(
+            //icon: DSIcons.arrow_drop_down_circle,
+
+            ),
+        inputDecoration: const InputDecoration(
+          border: InputBorder.none,
+        ),
+        cursorColor: DSColors.primaryMain,
+        selectorTextStyle: const TextStyle(
+          color: DSColors.neutralMediumElephant,
+        ),
+        textStyle:
+            const TextStyle(fontSize: 16.0, color: DSColors.neutralDarkCity),
+        spaceBetweenSelectorAndTextField: 8.0,
+        hintText: "teste",
+        initialValue: number, 
+        keyboardType:
+            const TextInputType.numberWithOptions(signed: true, decimal: true,),
+        textFieldController: controller,
+        maxLength: 15,
+        onInputChanged: (PhoneNumber number) {
+          number.phoneNumber;
+        },
+      ),
+    );
   }
 }
