05dd046bb4d2eda92853a9adb190048b7b11c348
Paolla Lira <paollalira@outlook.com>
2023-04-26 16:54:04 -0300

    Flags and countries json

---
+++ lib/src/models/ds_input_phone.model.dart
@@ -0,0 +1,5 @@
+class DSInputPhoneModel {
+  var code;
+  String? name;
+
+}
--- lib/src/widgets/fields/ds_input_phone.widget.dart
+++ lib/src/widgets/fields/ds_input_phone.widget.dart
@@ -1,62 +1,81 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:blip_ds/blip_ds.dart';
 import 'package:flutter/material.dart';
-import 'package:intl_phone_number_input/intl_phone_number_input.dart';
-import 'package:mask_text_input_formatter/mask_text_input_formatter.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
-//import 'package:blip_ds/src/widgets/fields/ds_input_decoration.widget.dart';
-
-class DSInputPhone extends StatelessWidget {
+class DSInputPhone extends StatefulWidget {
   final String? hintText;
-  final String initialCountry = 'BR';
-  final PhoneNumber number = PhoneNumber(isoCode: 'BR');
-  final TextEditingController controller = TextEditingController();
-  DSInputPhone({
+
+  const DSInputPhone({
     super.key,
     this.hintText,
   });
 
   @override
+  State<DSInputPhone> createState() => _DSInputPhoneState();
+}
+
+class _DSInputPhoneState extends State<DSInputPhone> {
+  List<DropdownMenu> list = ;
+  String dropDownValue = list.first;
+  // final String initialCountry = 'BR';
+
+  // final PhoneNumber number = PhoneNumber(isoCode: 'BR');
+
+  // final TextEditingController controller = TextEditingController();
+
+  @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: const BoxDecoration(
-        border: Border(
-          bottom: BorderSide(color: DSColors.neutralMediumWave),
-          top: BorderSide(color: DSColors.neutralMediumWave),
-          left: BorderSide(color: DSColors.neutralMediumWave),
-          right: BorderSide(color: DSColors.neutralMediumWave),
+        decoration: const BoxDecoration(
+          border: Border(
+            bottom: BorderSide(color: DSColors.neutralMediumWave),
+            top: BorderSide(color: DSColors.neutralMediumWave),
+            left: BorderSide(color: DSColors.neutralMediumWave),
+            right: BorderSide(color: DSColors.neutralMediumWave),
+          ),
+          borderRadius: BorderRadius.all(
+            Radius.circular(8.0),
+          ),
         ),
-        borderRadius: BorderRadius.all(
-          Radius.circular(8.0),
-        ),
-      ),
-      child: InternationalPhoneNumberInput(
-        inputDecoration: const InputDecoration(
-          hintText: "Número de telefone",
-          hintStyle: DSBodyTextStyle(color: DSColors.neutralMediumWave),
-          border: InputBorder.none,
-        ),
-        cursorColor: DSColors.primaryMain,
-        selectorTextStyle: const TextStyle(
-          color: DSColors.neutralMediumElephant,
-        ),
-        spaceBetweenSelectorAndTextField: 8.0,
-        initialValue: number,
-        //keyboardType: TextInputType.phone,
-        //   signed: true,
-        //   decimal: true,
+        child: Row(
+          children: [
+            DropdownButton<String>(
+                icon: SvgPicture.asset(
+                  'assets/svg/brazil_flag.svg',
+                ),
+                items: items,
+                onChanged: (String? value) {
+                  setState(() {
+                    dropDownValue = value!;
+                  });
+                }),
+                const TextField(keyboardType: TextInputType.phone,showCursor: true,cursorColor: DSColors.primaryMain,),
+          ],
+        )
 
-        textFieldController: controller,
-        maxLength: 15,
-        onInputChanged: (PhoneNumber number) {
-          number.phoneNumber;
-        },
-      ),
-    );
+        // InternationalPhoneNumberInput(
+        //   inputDecoration: const InputDecoration(
+        //     hintText: "Número de telefone",
+        //     hintStyle: DSBodyTextStyle(color: DSColors.neutralMediumWave),
+        //     border: InputBorder.none,
+        //   ),
+        //   cursorColor: DSColors.primaryMain,
+        //   selectorTextStyle: const TextStyle(
+        //     color: DSColors.neutralMediumElephant,
+        //   ),
+        //   spaceBetweenSelectorAndTextField: 8.0,
+        //   initialValue: number,
+        //   //keyboardType: TextInputType.phone,
+        //   //   signed: true,
+        //   //   decimal: true,
+
+        //   textFieldController: controller,
+        //   maxLength: 15,
+        //   onInputChanged: (PhoneNumber number) {
+        //     number.phoneNumber;
+        //   },
+        // ),
+        );
   }
-
-  final maskFormatter = MaskTextInputFormatter(
-      mask: '+# (###) ###-##-##',
-      filter: {"#": RegExp(r'[0-9]')},
-      type: MaskAutoCompletionType.lazy);
 }
