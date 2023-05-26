55d30567303ae57efc2565a7552b0305d5190df9
RaulRodrigo06 <raulrodrigo6@gmail.com>
2023-05-26 10:14:20 -0300

    feat: adding dashed border lib and using

--- lib/src/widgets/borders/ds_dashed_border.widget.dart
+++ lib/src/widgets/borders/ds_dashed_border.widget.dart
@@ -1,5 +1,4 @@
-import 'dart:math' as math;
-
+import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
 
 import '../../../blip_ds.dart';
@@ -16,103 +15,17 @@ class DSDashedBorder extends StatelessWidget {
   final Widget child;
   @override
   Widget build(BuildContext context) {
-    return CustomPaint(
-      painter: _DashRectPainter(
+    return Padding(
+      padding: const EdgeInsets.all(1),
+      child: DottedBorder(
         color: borderColor,
         strokeWidth: strokeWidth,
-        gap: 3,
+        radius: const Radius.circular(8),
+        borderType: BorderType.RRect,
+        dashPattern: const [3, 3],
+        padding: EdgeInsets.zero,
+        child: child,
       ),
-      child: child,
     );
-  }
-}
-
-class _DashRectPainter extends CustomPainter {
-  double strokeWidth;
-  Color color;
-  double gap;
-
-  _DashRectPainter(
-      {this.strokeWidth = 5.0, this.color = Colors.red, this.gap = 5.0});
-
-  @override
-  void paint(Canvas canvas, Size size) {
-    Paint dashedPaint = Paint()
-      ..color = color
-      ..strokeWidth = strokeWidth
-      ..style = PaintingStyle.stroke;
-
-    double x = size.width;
-    double y = size.height;
-
-    Path topPath = getDashedPath(
-      a: const math.Point(0, 0),
-      b: math.Point(x, 0),
-      gap: gap,
-    );
-
-    Path rightPath = getDashedPath(
-      a: math.Point(x, 0),
-      b: math.Point(x, y),
-      gap: gap,
-    );
-
-    Path bottomPath = getDashedPath(
-      a: math.Point(0, y),
-      b: math.Point(x, y),
-      gap: gap,
-    );
-
-    Path leftPath = getDashedPath(
-      a: const math.Point(0, 0),
-      b: math.Point(0.001, y),
-      gap: gap,
-    );
-
-    canvas.drawPath(topPath, dashedPaint);
-    canvas.drawPath(rightPath, dashedPaint);
-    canvas.drawPath(bottomPath, dashedPaint);
-    canvas.drawPath(leftPath, dashedPaint);
-  }
-
-  Path getDashedPath({
-    required math.Point<double> a,
-    required math.Point<double> b,
-    required gap,
-  }) {
-    Size size = Size(b.x - a.x, b.y - a.y);
-    Path path = Path();
-    path.moveTo(a.x, a.y);
-    bool shouldDraw = true;
-    math.Point currentPoint = math.Point(a.x, a.y);
-
-    num radians = math.atan(size.height / size.width);
-
-    num dx = math.cos(radians) * gap < 0
-        ? math.cos(radians) * gap * -1
-        : math.cos(radians) * gap;
-
-    num dy = math.sin(radians) * gap < 0
-        ? math.sin(radians) * gap * -1
-        : math.sin(radians) * gap;
-
-    while (currentPoint.x <= b.x && currentPoint.y <= b.y) {
-      shouldDraw
-          ? path.lineTo(double.parse(currentPoint.x.toString()),
-              double.parse(currentPoint.y.toString()))
-          : path.moveTo(double.parse(currentPoint.x.toString()),
-              double.parse(currentPoint.y.toString()));
-      shouldDraw = !shouldDraw;
-      currentPoint = math.Point(
-        currentPoint.x + dx,
-        currentPoint.y + dy,
-      );
-    }
-    return path;
-  }
-
-  @override
-  bool shouldRepaint(CustomPainter oldDelegate) {
-    return true;
   }
 }
