bdb764fd3faea770e9a6c1f6ed2357ded64755b2
Marcelo Amaro <marcelo.amaro@take.net>
2023-11-01 10:42:11 -0300

    fix: get extension from mime type when returning cache path

--- lib/src/utils/ds_directory_formatter.util.dart
+++ lib/src/utils/ds_directory_formatter.util.dart
@@ -3,6 +3,8 @@ import 'dart:io';
 import 'package:get/get.dart';
 import 'package:path_provider/path_provider.dart';
 
+import '../services/ds_file.service.dart';
+
 abstract class DSDirectoryFormatter {
   static Future<String> getCachePath({
     required final String type,
@@ -14,7 +16,7 @@ abstract class DSDirectoryFormatter {
     final typeComponents = type.split('/');
 
     final typeFolder = '${typeComponents.first.capitalizeFirst}';
-    extension ??= typeComponents.last;
+    extension ??= DSFileService.getFileExtensionFromMime(type);
 
     final typePrefix = '${typeFolder.substring(0, 3).toUpperCase()}-';
 
