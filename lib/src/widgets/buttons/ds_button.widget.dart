48dfc0357e1573738b0a0accfc659ff7d3650c38
Marcelo Amaro <marcelo.amaro@take.net>
2024-09-08 21:45:57 -0300

    test: update goldens

--- lib/src/widgets/buttons/ds_button.widget.dart
+++ lib/src/widgets/buttons/ds_button.widget.dart
@@ -73,7 +73,7 @@ class DSButton extends StatelessWidget {
             ? BorderSide(
                 color: borderColor!,
               )
-            : null,
+            : BorderSide.none,
       ),
       child: DSAnimatedSize(
         child: Row(
