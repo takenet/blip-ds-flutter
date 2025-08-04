3ee9c093229eb5ee2bc8db36286c55d8fb120989
Andre Rossi <andre.rossi@blip.ai>
2025-08-04 14:25:58 -0300

    feat: add new colors

--- lib/src/themes/colors/ds_dark_colors.theme.dart
+++ lib/src/themes/colors/ds_dark_colors.theme.dart
@@ -5,7 +5,9 @@ abstract class DSDarkColors {
   static const Color surfacePositive = Color(0xFF01562F);
   static const Color surface0 = Color(0xFF424242);
   static const Color surface1 = Color(0xFF393939);
+  static const Color surface2 = Color(0xFF1f1f1f);
   static const Color surface3 = Color(0xFF141414);
+  static const Color surface4 = Color(0xFF0a0a0a);
   static const Color contentDefault = Color(0xFFFFFFFF);
   static const Color success = Color(0xFF355E4B);
   static const Color positive = Color(0xFF6BFFBC);
